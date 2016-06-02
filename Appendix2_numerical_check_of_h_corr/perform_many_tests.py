e4a929a307aac7634d863160d251c8c463970761
Tommaso Comparin <comparin@lps.ens.fr>
2016-06-02 18:57:43 +0200

    better tests

--- Appendix2_numerical_check_of_h_corr/perform_many_tests.py
+++ Appendix2_numerical_check_of_h_corr/perform_many_tests.py
@@ -9,7 +9,7 @@ import random
 from hcorr_integrals_cy import HCorr as HCorr_cy
 
 def random_vector():
-    return [round(random.uniform(-3.0, 3.0), 5) for i in [0, 1, 2]]
+    return [round(random.uniform(-2.0, 2.0), 5) for i in [0, 1, 2]]
 
 
 ntests = 20
@@ -19,6 +19,6 @@ for itest in range(ntests):
     B = random_vector()
     E = random_vector()
     k = random_vector()
-    beta_star = round(random.uniform(0.1, 2.0), 5)
+    beta_star = round(random.uniform(0.4, 2.5), 5)
     HC = HCorr_cy(A, B, E, beta_star)
     HC.full_check(k, ID='test_%02i' % itest)
